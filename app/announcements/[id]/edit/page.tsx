2d4250d9fcb62d08e6cc6a1a337b9af8c64455a4
Artur Kret <arturkret44@gmail.com>
2026-04-01 22:59:01 +0200

    Update page.tsx

--- app/announcements/[id]/edit/page.tsx
+++ app/announcements/[id]/edit/page.tsx
@@ -3,7 +3,7 @@
 import { supabaseServer } from "@/lib/supabase/server";
 import { requireApprovedUser } from "@/lib/guards";
 import { redirect } from "next/navigation";
-import { EditAnnouncementForm } from "./form";
+import { EditAnnouncementForm } from "@/app/announcements/new/form";
 import { CreateAnnouncementForm } from "@/app/announcements/new/form";
 export default async function EditAnnouncementPage({
   params,
